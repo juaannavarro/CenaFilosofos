ab90376e973d7246a4e6928cb6f70f9112a0b876
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2023-05-18 18:20:16 +0200

    cambio

--- filosofos.py
+++ filosofos.py
@@ -51,22 +51,10 @@ class Filosofo(threading.Thread):
         
     
 
-def main():
-    num_filosofos = 5
-    max_comidas = 4
-    tenedores = [threading.Lock() for n in range(num_filosofos)]
-    filosofos = [Filosofo(f'Filosofo {n}', tenedores[n%num_filosofos], tenedores[(n+1)%num_filosofos], max_comidas) 
-                 for n in range(num_filosofos)]
-        
-    for filosofo in filosofos:
-        filosofo.start()
-        
-    for filosofo in filosofos:
-        filosofo.join()
+
         
     
-if __name__ == '__main__':
-    main()
+
     
 
         
